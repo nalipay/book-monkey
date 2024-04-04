eae99ad419a5f2470af27a0c1170696e870307a9
Meryl Kirchhoff <meryl.kirchhoff@gmx.de>
2024-04-04 19:56:54 +0200

    components

---
+++ src/app/book-list/book-list.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Book } from '../shared/book';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent {
+  books: Book[] = [];
+
+  constructor() {
+    this.books = [
+      {
+        isbn: '12345',
+        title: 'Tierisch gut kochen',
+        authors: ['Mrs Chimp', 'Mr Gorilla'],
+        published: '2022-06-20',
+        subtitle: 'Rezepte von Affen bis Zebra',
+        thumbnailUrl: 'https://cdn.ng-buch.de/kochen.png',
+        description: 'Immer lecker und gut'
+      },
+      {
+        isbn: '67890',
+        title: 'Backen mit Affen',
+        authors: ['Orang Utan'],
+        published: '2022-07-15',
+        subtitle: 'Bananenbrot und mehr',
+        thumbnailUrl: 'https://cdn.ng-buch.de/backen.png',
+        description: 'Tolle Backtipps für Mensch und Tier'
+      }
+    ];
+  }
+}
